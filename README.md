8d3524b5716bd3b1099984aee7546680787654fb
jkoninger <jck999992@gmail.com>
2023-02-19 11:37:35 +0000

    Fixed regions, added requirements and example map.svg. Added Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Current Temperature of Capital Cities Around the World
+
+This project uses the [RESTCountries](https://restcountries.com/) and [OpenWeather](https://openweathermap.org/api) APIs,
+along with GeoPandas, to generate a world map of the capital cities of the world and their current temperature.
+
+Requirements can be found in `requirements.txt`, please also generate an OpenWeather API key and paste it into the top of
+the `main.py` file.
+
+When the script is run it generates a `map.svg` output showing the result, and also displays the map as a plot.
--- main.py
+++ main.py
@@ -4,29 +4,27 @@ import geopandas as gpd
 import requests
 from adjustText import adjust_text
 
-REGIONS = ['eur', 'afr', 'ame', 'asia', 'aus']
+REGIONS = ['eur', 'afr', 'ame', 'asia', 'oce']
 REST_COUNTRIES_ENDPOINT = "https://restcountries.com/v3.1/"
 OPEN_WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
-OPEN_WEATHER_KEY = "b6c463e3b83566fd97b08df8a6b9578a"
+OPEN_WEATHER_KEY = ""
 DEGREE_SIGN = u'\N{DEGREE SIGN}'
 
 
-def get_regional_data(regions):
+def get_regional_data():
     data = {}
-    if isinstance(regions, str):
-        if regions.lower() == 'all':
-            regions = REGIONS
+    for region in REGIONS:
+        region_data = requests.get(f"{REST_COUNTRIES_ENDPOINT}/region/{region}")
+        if region_data.status_code == 200:
+            for country in region_data.json():
+                try:
+                    data[country['name']['common']] = {'lat': country['capitalInfo']['latlng'][1],
+                                                   'long': country['capitalInfo']['latlng'][0], 'flag': country['flag'],
+                                                   'capital': country['capital'][0]}
+                except:
+                    continue
         else:
-            regions = [regions]
-    for region in regions:
-        region_data = requests.get(f"{REST_COUNTRIES_ENDPOINT}/region/{region}").json()
-        for country in region_data:
-            try:
-                data[country['name']['common']] = {'lat': country['capitalInfo']['latlng'][1],
-                                               'long': country['capitalInfo']['latlng'][0], 'flag': country['flag'],
-                                               'capital': country['capital'][0]}
-            except:
-                continue
+            raise requests.exceptions.RequestException(f"Invalid status code for request {region_data.url}, try again")
     return pd.DataFrame.from_dict(data)
 
 
@@ -43,25 +41,31 @@ def get_weather_data(capital_cities):
             data = response.json()
             temperature = data.get("main", {}).get("temp")
             weather_data[city_name] = temperature
+        if response.status_code == 401:
+            raise requests.exceptions.RequestException("Invalid API key. Please add a valid key to the top of this file")
     return weather_data
+
+
+def annotate_graph(ax, weather_data, capital_cities_coords):
+    texts = []
+    for capital, temp in weather_data.items():
+        texts.append(ax.text(s=f"{capital}: {temp}{DEGREE_SIGN}C", x=capital_cities_coords.get(capital)[0],
+                             y=capital_cities_coords.get(capital)[1], fontsize=2))
+    adjust_text(texts, arrowprops=dict(arrowstyle="-", color='r', lw=0.2))
 
 
 def plot_graph():
     fig, ax = plt.subplots(figsize=(24, 16))
     countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
     countries.plot(color="lightgrey", ax=ax)
-    country_data = get_regional_data('eur')
-    capital_cities = {values.get('capital'): [values.get('lat'), values.get('long')]
-                      for country, values in country_data.items()}
-    lats = [x[0] for x in capital_cities.values()]
-    longs = [x[1] for x in capital_cities.values()]
-    weather_data = get_weather_data(capital_cities.keys())
+    country_data = get_regional_data()
+    capital_cities_coords = {values.get('capital'): [values.get('lat'), values.get('long')]
+                             for country, values in country_data.items()}
+    lats = [x[0] for x in capital_cities_coords.values()]
+    longs = [x[1] for x in capital_cities_coords.values()]
+    weather_data = get_weather_data(capital_cities_coords.keys())
     ax.scatter(lats, longs, s=0.5)
-    texts = []
-    for capital, temp in weather_data.items():
-        texts.append(ax.text(s=f"{capital}: {temp}{DEGREE_SIGN}C", x=capital_cities.get(capital)[0],
-                             y=capital_cities.get(capital)[1], fontsize=2))
-    adjust_text(texts, arrowprops=dict(arrowstyle="-", color='r', lw=0.5))
+    annotate_graph(ax, weather_data, capital_cities_coords)
     plt.savefig('map.svg')
     plt.show()
 
